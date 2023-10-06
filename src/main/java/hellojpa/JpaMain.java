ff81165c7fc0f3558d400ace1acdb454ed5e2b84
nyeroni <nir2y@naver.com>
2023-10-06 15:39:11 +0900

    Update: 실습

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -16,8 +17,15 @@ public class JpaMain {
 
 
         try {
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("UpdateMemberName");
+            //Member findMember = em.find(Member.class, 1L);
+            List<Member> result = em.createQuery("SELECT m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+            for(Member member:result){
+                System.out.println("member.getName() = " + member.getName());
+            }
+            
             tx.commit();
 
         }catch (Exception e){
