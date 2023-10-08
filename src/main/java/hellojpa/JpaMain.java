6d734ec1d53cfa31d1571c1af1307b04732f614d
nyeroni <nir2y@naver.com>
2023-10-08 16:57:04 +0900

    Update:  양방향 연관관계 예시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,25 +17,33 @@ public class JpaMain {
 
 
         try {
-
+            //저장
             Team team = new Team();
             team.setName("TeamA");
             em.persist(team);
 
             Member member = new Member();
             member.setUsername("member1");
-            member.setTeamId(team.getId());
+            member.setTeam(team);
             em.persist(member);
 
+            em.flush();
+            em.clear();
 
+            //조회
+            Member findMember = em.find(Member.class, member.getId());
 
+            List<Member> members = findMember.getTeam().getMembers();
+            for (Member m : members){
+                System.out.println("m.getUsername() = " + m.getUsername());
+            }
             tx.commit();
 
         }catch (Exception e){
             tx.rollback();
         }
         finally {
-            em.close();
+              em.close();
 
         }
 
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -12,8 +12,18 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
-    @Column(name = "TEAM_ID")
-    private Long teamId;
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    public Team getTeam() {
+        return team;
+    }
+
+
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
     public Long getId() {
         return id;
@@ -30,12 +40,8 @@ public class Member {
     public void setUsername(String username) {
         this.username = username;
     }
-
-    public Long getTeamId() {
-        return teamId;
+    public void setTeam(Team team) {
+        this.team = team;
     }
 
-    public void setTeamId(Long teamId) {
-        this.teamId = teamId;
-    }
 }
