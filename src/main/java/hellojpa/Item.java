e8b177996c7d04b5ed3820be7fd5f2450822174d
nyeroni <nir2y@naver.com>
2023-10-10 18:43:15 +0900

    Update:  상속 매핑 관련 엔티티 설정

---
+++ src/main/java/hellojpa/Item.java
@@ -0,0 +1,14 @@
+package hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+public class Item {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private int price;
+}
