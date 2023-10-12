9515cbe1a9fa4b7536342c46cce8bacb49fdf07f
nyeroni <nir2y@naver.com>
2023-10-12 22:13:35 +0900

    Update:  구현 클래스마다 테이블 전략

--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @DiscriminatorColumn
-public class Item {
+public abstract class Item {
 
     @Id @GeneratedValue
     private Long id;
