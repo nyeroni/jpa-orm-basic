51518009459090924215141e1c8e0a6d0a8e0cd7
nyeroni <nir2y@naver.com>
2023-10-17 16:57:04 +0900

    Update: 지연로딩 예시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,24 +16,25 @@ public class JpaMain {
 
 
         try {
-
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            em.persist(member1);
-
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            em.persist(member2);
+             
+            Team team  = new Team();
+            team.setName("teamA");
+            em.persist(team);
+            
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeam(team);
+            em.persist(member);
+            
 
             em.flush();
             em.clear();
+            
+            
+            Member m = em.find(Member.class, member.getId());
 
+            System.out.println("m.getTeam().getCreatedBy() = " + m.getTeam().getCreatedBy());
 
-            Member m1 = em.find(Member.class, member1.getId());
-            Member m2 = em.getReference(Member.class, member2.getId());
-
-
-            System.out.println("m1 == m2 : " + (m1.getClass() == m2.getClass()));
             tx.commit();
 
         }catch (Exception e){
